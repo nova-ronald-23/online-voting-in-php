5c5076b6c55095991d24080568a9c6a16054adac
nova ronald s <69787583+nova-ronald-23@users.noreply.github.com>
2023-07-21 16:03:21 +0530

    nova change

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
     <head></head>
     <body>
-    
+    <?php include :'header.php'?>
 <form method="post" action="">
     <input type="email" name="email" placeholder="Email" required>
     <input type="password" name="password" placeholder="Password" required>
